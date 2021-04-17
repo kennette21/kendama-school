1a9dee4cfb954eea2cde1fc97ebddb7b5efb7bcb
Thomas Bean <kennette21@gmail.com>
2021-04-17 22:21:43 +0200

    make absolutely SICK kendflip animation with css

---
+++ src/Playground.tsx
@@ -0,0 +1,71 @@
+// import React, { useEffect, useState } from "react";
+import "./App.css";
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as Dama } from "./graphics/damadarkColors.svg";
+import { ReactComponent as Ken } from "./graphics/KendarkColors.svg";
+
+const damaKenFlip = keyframes`
+    0%, 10% {
+        transform: translateY(65px) translateX(4px) rotate(5deg);
+    }
+    20% {
+        transform: rotate(-10deg) translateY(0px) translateX(-3px);
+    }
+    70% {
+        transform: rotate(-360deg) translateX(0);
+    }
+    100% {
+        transform: translateY(65px) translateX(4px) rotate(5deg);
+    }
+`;
+
+const kenKenFlip = keyframes`
+    0%, 10% {
+        transform: translateY(0px) translateX(-3px) rotate(5deg);
+    }
+    20% {
+        transform: translateY(-20px) rotate(-5deg);
+    }
+    70% {
+        transform: translateY(-10px) rotate(0deg);
+    }
+    100%{
+        transform: translateY(0px) translateX(-3px) rotate(5deg);
+    }
+`;
+
+const StyledDama = styled(Dama)`
+	position: absolute;
+	top: -80px;
+	left: 10px;
+	z-index: 100;
+	animation: ${damaKenFlip} 2s ease infinite;
+`;
+const StyledKen = styled(Ken)`
+	position: absolute;
+	animation: ${kenKenFlip} 2s ease infinite;
+`;
+const Container = styled.div`
+	display: flex;
+	width: 100%;
+	height: 500px;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+`;
+const AnimationContianer = styled.div`
+	position: relative;
+`;
+
+function Playground() {
+	return (
+		<Container>
+			<AnimationContianer>
+				<StyledDama />
+				<StyledKen />
+			</AnimationContianer>
+		</Container>
+	);
+}
+
+export default Playground;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Playground from "./Playground";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>{true ? <Playground /> : <App />}</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
