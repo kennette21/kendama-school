af78f07c1788e38f6ca4405d9097875f6ed3d3c9
Thomas Bean <kennette21@gmail.com>
2021-04-18 09:42:39 +0200

    some descriptive comments

--- src/Kenflip.tsx
+++ src/Kenflip.tsx
@@ -5,36 +5,36 @@ import { ReactComponent as Ken } from "./graphics/KendarkColors.svg";
 
 const damaKenFlip = keyframes`
     0%, 10% {
-        transform: translateY(65px) translateX(4px) rotate(8deg);
+        transform: translateY(65px) translateX(4px) rotate(8deg); // start on the spike, tilted
     }
     20% {
-        transform: rotate(-10deg) translateY(0px) translateX(-3px);
+        transform: rotate(-10deg) translateY(0px) translateX(-3px); // put svg in home Y position
     }
     70% {
-        transform: rotate(-360deg) translateX(0);
+        transform: rotate(-360deg) translateX(0); // rotate once svg is in home X and Y position
     }
     100% {
-        transform: translateY(65px) translateX(4px) rotate(8deg);
+        transform: translateY(65px) translateX(4px) rotate(8deg); // end on the spike, tilted
     }
 `;
 
 const kenKenFlip = keyframes`
     0%, 10% {
-        transform: translateY(0px) translateX(-3px) rotate(5deg);
+        transform: translateY(0px) translateX(-3px) rotate(5deg); // start tilted
     }
     20% {
-        transform: translateY(-20px) rotate(-5deg);
+        transform: translateY(-20px) rotate(-5deg); // move up and flick the ken back
     }
     70% {
-        transform: translateY(-10px) rotate(0deg);
+        transform: translateY(-10px) rotate(0deg); // move down and come back to center, preparing to catch
     }
     100%{
-        transform: translateY(0px) translateX(-3px) rotate(5deg);
+        transform: translateY(0px) translateX(-3px) rotate(5deg); // end tilted
     }
 `;
 
 const StyledDama = styled(Dama)`
-	position: absolute;
+	position: absolute; // must position absolute to have the Tama sit over the ken
 	top: -80px;
 	left: 10px;
 	z-index: 100;
